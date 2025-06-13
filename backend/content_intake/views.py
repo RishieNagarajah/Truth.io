6e7ce0a51723593374293bb0dd1a9c22dff387f8
Jerry <88013020+MingLongSu@users.noreply.github.com>
2025-06-12 22:44:10 -0400

    Added content-intake app to backend

---
+++ backend/content_intake/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+
+    request = 'TODO: implement sanitization and data parsing logic here'
+
+    return HttpResponse(request)
