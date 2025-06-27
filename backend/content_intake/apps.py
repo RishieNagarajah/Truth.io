64266c1d25eabe6a1a5f4e5e39ce5ccbb9d74629
Jerry <88013020+MingLongSu@users.noreply.github.com>
2025-06-26 22:47:50 -0400

    process content v2

--- backend/content_intake/apps.py
+++ backend/content_intake/apps.py
@@ -1,6 +1,11 @@
 import spacy
 from django.apps import AppConfig
+from keybert import KeyBERT
+from sentence_transformers import SentenceTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 
+keyword_model = None
+sentence_model = None
 nlp_model_spaCy = None
 class ContentIntakeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -8,15 +13,23 @@ class ContentIntakeConfig(AppConfig):
 
     def ready(self):
         """
-        Perform spaCy model load once
+        Perform model loads once
         """
         global nlp_model_spaCy 
+        global sentence_model
+        global keyword_model
 
         if (nlp_model_spaCy == None):
             try:
                 # Attempt to load the large English language model 
                 nlp_model_spaCy = spacy.load('en_core_web_lg')
                 print('spaCy model "en_core_web_lg" loaded successfully!')
+
+                # Load sentence model 
+                sentence_model = SentenceTransformer('all-MiniLM-L6-v2')
+                keyword_model = KeyBERT(model=sentence_model)
+                print('Keyword model "all-MiniLM-L6-v2" loaded successfully!')
+
             except OSError:
                 print('Error: Language model "en_core_web_lg" not found.')
                 print('Please ensure you have run "python -m spacy download en_core_web_lg" in your active Conda environment.')
